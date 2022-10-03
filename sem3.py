bbe192edf894a06119223fe61e3a1e595a7e10b5
iGolubS <109303592+iGolubS@users.noreply.github.com>
2022-10-03 16:42:03 +0300

    Add files via upload

---
+++ sem3.py
@@ -0,0 +1,123 @@
+# выпавших Решек.
+
+
+# tosses = input('Введите результат подбрасываний: ')
+# repeats_headds = 0
+# max_repeats = 0
+# for i in range(len(tosses)-1):
+#     if (tosses[i]=='Р' and tosses[i+1]=='Р'):
+#         repeats_headds+=1
+#     elif (tosses[i]=='Р' and tosses[i+1]=='О'):
+#         if max_repeats<repeats_headds:
+#             max_repeats=repeats_headds
+#         repeats_headds=0
+# print(max_repeats+1)
+
+
+
+# n = input("Введите результат выпадения Орла и решки:")
+# total = 0
+# max = 0
+# for i in n:
+#     if i == 'Р':
+#         total += 1
+#         if total > max:
+#             max = total
+#     else:
+#         if total > max:
+#             max = total
+#         total = 0
+# print(max)
+
+
+
+
+
+# s=input()
+# t=0
+# while "Р"*(t+1) in s:
+#     t+=1
+# if t!=0:
+#     print(t)
+# else:
+#     print(0)
+
+
+
+
+
+
+
+
+# array = ['osfjwoiergwoignaewpjofwoeijfnwfonewfoignewtowenffnoeiwowjfninoiwfen', 'anton', 'aoooooooooontooooo', 'elelelelelelelelelel', 'ntoneeee', 'tonee', '253235235a5323352n25235352t253523523235oo235523523523n', 'antoooooooooooooooooooooooooooooooooooooooooooooooooooon', 'unton']
+# number = 252
+# for word in array:
+#     if str(number) in word:
+#         print(True)
+#     else:
+#         print(False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input())
+f=[]
+hacker =  ['a', 'n', 't', 'o', 'n','']
+count=0
+otvet=[]
